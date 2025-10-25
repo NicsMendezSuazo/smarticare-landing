df5491ed2ff3bc0453001c933df4493718fdf2b8
tmae20 <t.luna.534213@umindanao.edu.ph>
2025-10-25 16:45:28 +0800

    Fix base href and force add web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "345123bd1bbc5468dd04490f961e6813
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5a2e284b79968a79c3116ec1983ae4ff",
+"flutter_bootstrap.js": "b77dc48734804fbe390fe18eca643e13",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
